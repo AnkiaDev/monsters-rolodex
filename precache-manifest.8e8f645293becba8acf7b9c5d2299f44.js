8eb39f7498cae5706739f87ef1a4a00907cfb85f
AnkiaDev <ankiadev@gmail.com>
2019-10-07 11:21:49 +0200

    Updates

---
+++ precache-manifest.8e8f645293becba8acf7b9c5d2299f44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9644072caa56b53314c9a26eec21de3d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "73101762407914995607",
+    "url": "/monsters-rolodex/static/css/main.0a336579.chunk.css"
+  },
+  {
+    "revision": "0f45ce67b0a13b5d1fa5",
+    "url": "/monsters-rolodex/static/js/2.009e4c1d.chunk.js"
+  },
+  {
+    "revision": "73101762407914995607",
+    "url": "/monsters-rolodex/static/js/main.d4d1df87.chunk.js"
+  },
+  {
+    "revision": "e8e91111d9f0909e5242",
+    "url": "/monsters-rolodex/static/js/runtime-main.fe1a56d9.js"
+  }
+]);
